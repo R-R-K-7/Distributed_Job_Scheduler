e20c61f797b382b7494a88afac8c013667c2440b
RisheeKeshavan <r.keshavanrishee@gmail.com>
2026-05-18 21:38:51 +0530

    Created a basic server for submmitting jobs and checking their status.

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const {v4 : uuidv4} = require('uuid');
+
+// define app
+const app = express();
+
+app.use(express.json());
+
+// create map to hold the meta-data of jobs
+const jobs = new Map();
+
+// submit a job
+app.post('/submit', (req, res) => {
+	const body = req.body;
+	const job = {
+				"id" : uuidv4(),
+				"status" : "QUEUED",
+				"code" : body.code,
+				"lang" : body.lang,
+				"output" : null,
+				"created" : new Date(),
+			};
+	jobs.set(job.id, job);
+	return res.status(202).json({
+			message : "Successfully created Job",
+			jobId : job.id
+	});
+});
+
+// track job status
+app.get('/status/:jobid', (req, res) => {
+	const {jobid} = req.params;
+	if (!jobs.has(jobid)){
+		return res.status(404).send(`Job Id : ${jobid} not found.`);
+	}
+	const {code, ...stat} = jobs.get(jobid);
+	return res.status(200).json(stat);
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000...");
+});
